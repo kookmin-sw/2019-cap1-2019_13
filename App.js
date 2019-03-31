2fb0cb2d8e61ae86138d3eb1ea36abdadeccb360
jYaeEun <33084041+jYaeEun@users.noreply.github.com>
2019-04-01 02:24:19 +0900

    2차구현 (미완성)

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import {Image, View, Text ,Alert} from 'react-native';
-import Button from 'apsl-react-native-button';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {TouchableOpacity,StyleSheet} from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation'; 
 
@@ -9,17 +7,27 @@ class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'  }}>
-        <Button
-          style={{backgroundColor: 'orange',border: 30}}  
-          onPress={() => this.props.navigation.navigate('quiz')}> 
-         <Text style={{fontSize : 30,color:'white'}}>QUIZ</Text>
-        </Button>
+        
+        <TouchableOpacity style={{backgroundColor:'orange',borderRadius: 5 ,margin:50,padding:15}}
+                      onPress={() => this.props.navigation.navigate('quiz1')} >
+          <Text style ={{fontSize:30,color:'white'}}>자음 QUIZ </Text>
+        </TouchableOpacity>
+         <TouchableOpacity style={{backgroundColor:'orange',borderRadius: 5 , margin:50,padding:15}}
+                      onPress={() => this.props.navigation.navigate('quiz2')} >
+          <Text style ={{fontSize:30,color:'white'}}>모음 QUIZ </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={{backgroundColor:'orange',borderRadius: 5 , margin:50,padding:15}}
+                      onPress={() => this.props.navigation.navigate('quiz3')} >
+          <Text style ={{fontSize:30,color:'white'}}>약어 QUIZ </Text>
+        </TouchableOpacity>
+
+
       </View>
     );
   }
 }
 
-class quizscreen extends React.Component {
+class quiz1screen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' , margin:10}}>
@@ -32,7 +40,7 @@ class quizscreen extends React.Component {
         </TouchableOpacity>
         <Text
         style = {{fontSize:40,position:'absolute',top:170,}}>
-        "ㄱ"는 무엇인가요? 
+        초성 "ㄱ"는? 
         </Text> 
        <TouchableOpacity style={{backgroundColor:'orange',position: 'absolute',top:300,left:60,paddingTop:10,                 paddingBottom:10, paddingLeft:20, paddingRight:20, borderRadius: 10}}>
           <Text style ={{fontSize:20,color:'white' }}>1</Text>
@@ -59,10 +67,94 @@ class quizscreen extends React.Component {
   }
 }
 
+class quiz2screen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' , margin:10}}>
+        <Text
+          style={{padding: 10,fontSize: 40,fontWeight: 'bold',position: 'absolute',top: 20,  }}>
+          QUIZ!</Text>
+        <TouchableOpacity style={{backgroundColor:'orange',position: 'absolute',top:40,right:60,borderRadius: 5 }}>
+          <Text
+          style ={{fontSize:30,color:'white'}}>next</Text>
+        </TouchableOpacity>
+        <Text
+        style = {{fontSize:40,position:'absolute',top:170,}}>
+        모음 "ㅓ"는? 
+        </Text> 
+       <TouchableOpacity style={{backgroundColor:'orange',position: 'absolute',top:300,left:60,paddingTop:10,                 paddingBottom:10, paddingLeft:20, paddingRight:20, borderRadius: 10}}>
+          <Text style ={{fontSize:20,color:'white' }}>1</Text>
+        </TouchableOpacity>
+        
+       <TouchableOpacity style={{backgroundColor:'orange',position: 'absolute',top:300 , alignItems :'center',                paddingTop:10,paddingBottom:10, paddingLeft:20, paddingRight:20, borderRadius: 10}}>
+          <Text style ={{fontSize:20,color:'white' }}>2</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={{backgroundColor:'orange',position: 'absolute',top:300,right:60,paddingTop:10,               paddingBottom:10, paddingLeft:20, paddingRight:20, borderRadius: 10}}>
+          <Text style ={{fontSize:20,color:'white' }}>3</Text>
+        </TouchableOpacity>
+        
+      <Image style={{ position:'absolute' , top:360,left:50 }} source={require('./u.png')} />
+      <Image style={{ position:'absolute' , top:360, alignItems :'center' }}  source={require('./wa.png')}/>
+      <Image style={{ position:'absolute' , top:360,right:50 }}  source={require('./was.png')}/>
+
+      <TouchableOpacity onPress={() => {Alert.alert('1번');}}  style={{backgroundColor:'orange',                            position:'absolute',alignItems:"center",paddingTop:10,paddingBottom:10, paddingLeft:20, paddingRight:20,           borderRadius: 10 ,  bottom :80   }}>
+      <Text style ={{fontSize:20,color:'white' }}>정답보기</Text>
+        </TouchableOpacity>
+
+      </View>
+    );
+  }
+}
+class quiz3screen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' , margin:10}}>
+        <Text
+          style={{padding: 10,fontSize: 40,fontWeight: 'bold',position: 'absolute',top: 20,  }}>
+          QUIZ!</Text>
+        <TouchableOpacity style={{backgroundColor:'orange',position: 'absolute',top:40,right:60,borderRadius: 5 }}>
+          <Text
+          style ={{fontSize:30,color:'white'}}>next</Text>
+        </TouchableOpacity>
+        <Text
+        style = {{fontSize:40,position:'absolute',top:170,}}>
+        약자"받침 ㅆ"는? 
+        </Text> 
+       <TouchableOpacity style={{backgroundColor:'orange',position: 'absolute',top:300,left:60,paddingTop:10,                 paddingBottom:10, paddingLeft:20, paddingRight:20, borderRadius: 10}}>
+          <Text style ={{fontSize:20,color:'white' }}>1</Text>
+        </TouchableOpacity>
+        
+       <TouchableOpacity style={{backgroundColor:'orange',position: 'absolute', top:300 , alignItems :'center',                paddingTop:10,paddingBottom:10, paddingLeft:20, paddingRight:20, borderRadius: 10}}>
+          <Text style ={{fontSize:20,color:'white' }}>2</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={{backgroundColor:'orange',position: 'absolute',top:300,right:60,paddingTop:10,               paddingBottom:10, paddingLeft:20, paddingRight:20, borderRadius: 10}}>
+          <Text style ={{fontSize:20,color:'white' }}>3</Text>
+        </TouchableOpacity>
+        
+      <Image style={{ position:'absolute' , top:360,left:50 }} source={require('./gi.png')} />
+      <Image style={{ position:'absolute' , top:360, alignItems :'center' }}  source={require('./ni.png')}/>
+      <Image style={{ position:'absolute' , top:360,right:50 }}  source={require('./siss.png')}/>
+
+      <TouchableOpacity onPress={() => {Alert.alert('3번');}}  style={{backgroundColor:'orange',                            position:'absolute',alignItems:"center",paddingTop:10,paddingBottom:10, paddingLeft:20, paddingRight:20,           borderRadius: 10 ,  bottom :80   }}>
+      <Text style ={{fontSize:20,color:'white' }}>정답보기</Text>
+        </TouchableOpacity>
+
+      </View>
+    );
+  }
+}
+
+
+
 const RootStack = createStackNavigator(
   {
     Home: HomeScreen,
-    quiz: quizscreen,
+    quiz1: quiz1screen,
+    quiz2: quiz2screen,
+    quiz3:quiz3screen
+
     },
   {
     initialRouteName: 'Home',
