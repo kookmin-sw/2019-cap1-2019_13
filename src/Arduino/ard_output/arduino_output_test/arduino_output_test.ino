ed11cb261c2d7c892fb08227e00e977eb96f651d
no7rosicky <no4cesc@nate.com>
2019-04-14 19:11:00 +0900

    bluetooth test
    bluetooth module test code update

--- src/Arduino/ard_output/arduino_output_test/arduino_output_test.ino
+++ src/Arduino/ard_output/arduino_output_test/arduino_output_test.ino
@@ -1,113 +1,91 @@
 #include <SoftwareSerial.h>
-#include <stdlib.h>
 
 int SolenoidOne = 11;
 int SolenoidTwo = 12;
-int Switch = 9;
-char SolOneStatus =LOW;
-char SolTwoStatus =LOW; 
-const int Tx = 2;
-const int Rx = 3;
-
+char SolOneStatus = LOW;
+char SolTwoStatus = LOW; 
+int Tx = 2;
+int Rx = 3;
+int Bluetooth = 0;
 SoftwareSerial BTSerial(Tx, Rx);   //bluetooth module Tx:Digital 2 Rx:Digital 3   
 
 void setup() {
-
-  Serial.begin(9600);
-  BTSerial.begin(9600);
-  pinMode(SolenoidOne, OUTPUT);
+  Serial.begin(9600); //시리얼 통신 시작
+  BTSerial.begin(9600); //블루투스 통신 시작
+  pinMode(SolenoidOne, OUTPUT); 
   pinMode(SolenoidTwo, OUTPUT);
-  pinMode(Switch,INPUT_PULLUP);
 }
 
 void loop() {
   String str;
+  digitalWrite(SolenoidOne, SolOneStatus);
+  digitalWrite(SolenoidTwo, SolTwoStatus);
   if (BTSerial.available())
   {
-    Serial.print("Bluetooth connected");
-    //Serial.print(BTSerial.read());
-    digitalWrite(SolenoidOne, SolOneStatus);
-    digitalWrite(SolenoidTwo, SolTwoStatus);
+    if(!Bluetooth)
+    {
+      Serial.println("Bluetooth connected");
+      Bluetooth = 1;
+    }
     str = readSerial();
   }
-  //String str;
-  //digitalWrite(SolenoidOne, SolOneStatus);
-  //digitalWrite(SolenoidTwo, SolTwoStatus);
-  //str = readSerial();
   if(str == "")
   {
     //Serial.println("empty string");  
   }
-  else if(str=="s1 up")
+  else if(str=="1")
   {
-    Serial.println(str);
-    Serial.println("Solenoid One UP");
     SolOneStatus = HIGH;
-  }
-  else if(str=="s2 up")
-  {
     Serial.println(str);
-    Serial.println("Solenoid Two UP");
+    Serial.println("Solenoid 1 UP");
+  }
+  else if(str=="2")
+  {
     SolTwoStatus = HIGH;
+    Serial.println(str);
+    Serial.println("Solenoid 2 UP");
   }
   else if(str=="3")
   {
-    Serial.println(str);
-    Serial.println("Solenoid One & Two UP");
     SolOneStatus = HIGH;
     SolTwoStatus = HIGH;
+    Serial.println(str);
+    Serial.println("Solenoid 1 & 2 UP");
   }
   else if(str=="4")
   {
-    Serial.println(str);
-    Serial.println("Solenoid One DOWN");
     SolOneStatus = LOW;
+    Serial.println(str);
+    Serial.println("Solenoid 1 DOWN");
   }
   else if(str=="5")
   {
-    Serial.println(str);
-    Serial.println("Solenoid Two DOWN");
     SolTwoStatus = LOW;
+    Serial.println(str);
+    Serial.println("Solenoid 2 DOWN");
+  }
+  else if(str=="6")
+  {
+    SolOneStatus = LOW;
+    SolTwoStatus = LOW;
+    Serial.println(str);
+    Serial.println("Solenoid 1 & 2 DOWN");
   }
   else
   {
     Serial.println(str);
     Serial.println("error!");
   }
-  /*
-  if (Serial.available())
-  {
-    char val;
-    val = BTSerial.read();
-    Serial.print(val);
-    if(val=='a')
-    {
-      digitalWrite(Switch,HIGH);
-    }
-    BTSerial.write(Serial.read());
-  }
-  if(digitalRead(Switch)==LOW)
-  {
-    digitalWrite(Switch,HIGH);
-    delay(100);
-  }
-  else
-  {
-    digitalWrite(11,LOW);
-    delay(100);
-  }*/
 }
 
 String readSerial()   
 {   
    String str = "";   
    char ch;   
-
-    while( Serial.available() > 0 )   
-    {   
+   while( BTSerial.available() > 0 )   
+   {   
       ch = BTSerial.read();   
-      str.concat(ch);   
-      delay(10);  
-    }   
-    return str;     
+      str.concat(ch); 
+   }  
+   return str;     
 }
