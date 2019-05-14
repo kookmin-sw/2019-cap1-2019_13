f05e49d990d5e07aff53797f356ce47c3638b75d
no7rosicky <no4cesc@nate.com>
2019-05-14 13:32:02 +0900

    Update arduino_output_test.ino
    데이터수신, 저장, 초기화 함수 구현

--- src/Arduino/ard_output/arduino_output_test/arduino_output_test.ino
+++ src/Arduino/ard_output/arduino_output_test/arduino_output_test.ino
@@ -1,7 +1,8 @@
 #include <SoftwareSerial.h>
 
-int LeftSolenoid[6]={4,5,6,7,8,9};
-int RightSolenoid[6]={10,11,12,13,14,15};
+int LeftSolenoid[6]={4, 5, 6, 7, 8, 9};
+int RightSolenoid[6]={10, 11, 12, 13, 14, 15};
+int DataArray[55]={0};
 
 char LeftSol_Status[6]={LOW};
 char RightSol_Status[6]={LOW};
@@ -10,6 +11,7 @@ int DataLength = 0;
 int Tx = 2;
 int Rx = 3;
 int Bluetooth = 0;
+
 SoftwareSerial BTSerial(Tx, Rx);   //bluetooth module Tx:Digital 2 Rx:Digital 3   
 
 void setup() {
@@ -19,8 +21,8 @@ void setup() {
   DataLength = 0;
   for(int i = 0; i < 6; i++) //핀연결, 모터 상태 초기화
   {
-    pinMode(LeftSolenoid[i],OUTPUT);
-    pinMode(RightSolenoid[i],OUTPUT);
+    pinMode(LeftSolenoid[i], OUTPUT);
+    pinMode(RightSolenoid[i], OUTPUT);
     LeftSol_Status[i] = LOW;
     RightSol_Status[i] = LOW;
   }
@@ -28,7 +30,7 @@ void setup() {
 
 void loop() {
 
-  String str;
+  String DotData;
   for(int i = 0; i < 6; i++) //상태 업데이트
   {
     digitalWrite(LeftSolenoid[i], LeftSol_Status[i]);
@@ -42,125 +44,59 @@ void loop() {
       Serial.println("Bluetooth connected");
       Bluetooth = 1;
     }
-    str = readSerial();
+    DotData = ReceiveDot();
   }
+  StoreDot(DotData,DataLength);
 
-  if(str == "")
-  {
+}
 
-  }
-  else if(str=="1")
+String ReceiveDot()   
+{   
+  String TempString = ""; 
+  while(BTSerial.available()>0)   
+  {   
+    TempString = BTSerial.readStringUntil('F'); 
+  }  
+  DataLength = int(TempString[0])-48;
+  return TempString;     
+}
+
+void StoreDot(String Ddata, int Dlength)
+{
+  int count = Dlength*6;
+  int index = 1;
+  while(count)
   {
-    SolOneStatus = HIGH;
-   
-    Serial.println(str);
-    Serial.println("Solenoid 1 UP");
-  }
-  else if(str=="2")
-  {
-    SolTwoStatus = HIGH;
-    
-    Serial.println(str);
-    Serial.println("Solenoid 2 UP");
-  }
-  
-  else if(str=="3")
-  {
-    SolThreeStatus = HIGH;
-    Serial.println(str);
-    Serial.println("Solenoid 3 UP");
-  }
-  else if(str=="4")
-  {
-    SolFourStatus = HIGH;
-    Serial.println(str);
-    Serial.println("Solenoid 4 UP");
-  }
-  else if(str=="5")
-  {
-    SolFiveStatus = HIGH;
-    Serial.println(str);
-    Serial.println("Solenoid 5 UP");
-  }
-  else if(str=="6")
-  {
-    SolSixStatus = HIGH;
-    Serial.println(str);
-    Serial.println("Solenoid 6 UP");
-  }
-  else if(str=="0")
-  {
-    SolOneStatus = HIGH;
-    SolTwoStatus = HIGH;
-    SolThreeStatus = HIGH;
-    SolFourStatus = HIGH;
-    SolFiveStatus = HIGH;
-    SolSixStatus = HIGH;
-    Serial.println(str);
-    Serial.println("Solenoid All UP");
-  }
-  else if(str=="a")
-  {
-    SolOneStatus = LOW;
-    Serial.println(str);
-    Serial.println("Solenoid 1 DOWN");
-  }
-  else if(str=="b")
-  {
-    SolTwoStatus = LOW;
-    Serial.println(str);
-    Serial.println("Solenoid 2 DOWN");
-  }
-  else if(str=="c")
-  {
-    SolThreeStatus = LOW;
-    Serial.println(str);
-    Serial.println("Solenoid 3 DOWN");
-  }
-  else if(str=="d")
-  {
-    SolFourStatus = LOW;
-    Serial.println(str);
-    Serial.println("Solenoid 4 DOWN");
-  }
-  else if(str=="e")
-  {
-    SolFiveStatus = LOW;
-    Serial.println(str);
-    Serial.println("Solenoid 5 DOWN");
-  }
-  else if(str=="f")
-  {
-    SolSixStatus = LOW;
-    Serial.println(str);
-    Serial.println("Solenoid 6 DOWN");
-  }
-  else if(str=="g")
-  {
-    SolOneStatus = LOW;
-    SolTwoStatus = LOW;
-    SolThreeStatus = LOW;
-    SolFourStatus = LOW;
-    SolFiveStatus = LOW;
-    SolSixStatus = LOW;
-    Serial.println(str);
-    Serial.println("Solenoid All DOWN");
-  }
-  else
-  {
-    Serial.println(str);
-    Serial.println("error!");
+    DataArray[index]=int(Ddata[index])-48;
+    count -= 1;
+    index += 1;
   }
 }
 
-String readSerial()   
-{   
-   String strr = "";   
-   char ch;   
-   while( BTSerial.available() > 0 )   
-   {   
-      ch = BTSerial.read();   
-      strr.concat(ch); 
-   }  
-   return strr;     
+void PrintDot()
+{
+
+
+}
+
+void Reset()
+{
+  for(int i = 0; i < 55; i++)
+  {
+    DataArray[i] = 0;
+  }
+  Bluetooth = 0;
+  DataLength = 0;
+  delay(500);
+  for(int i = 0; i < 6; i++)
+  {
+    digitalWrite(LeftSolenoid[i], HIGH);
+    digitalWrite(RightSolenoid[i], HIGH);
+  }
+  delay(500);
+  for(int i = 0; i < 6; i++)
+  {
+    digitalWrite(LeftSolenoid[i], LOW);
+    digitalWrite(RightSolenoid[i], LOW);
+  }
 }
